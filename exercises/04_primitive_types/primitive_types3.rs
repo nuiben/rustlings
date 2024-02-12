633ba69b9fcb69c8d87ea22df1ad197f2618a8f9
Ben Porter <102399623+nuiben@users.noreply.github.com>
2024-02-11 20:37:31 -0500

    finished 04 - primitive types

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -5,15 +5,17 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    // let a: [i32; 100] = [1; 100]; // This fills the array with 100 elements, all equal 1
+    let a: Vec<i32> = (1..101).collect(); // Pushes the numbers 1-100 into the array.
+    println!("{}", a.len());
+    println!("{}", a[0]);
+    println!("{}", a[99]);
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
-        panic!("Array not big enough, more elements needed")
+        panic!("Array not big enough, more elements needed");
     }
 }
